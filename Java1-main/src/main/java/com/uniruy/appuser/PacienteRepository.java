7efdcb0d67b289306b7633b48a490afd1d42848e
Ryan <ryanr252@hotmail.com>
2025-11-11 23:41:24 -0300

    Delete Java1-main directory

--- Java1-main/src/main/java/com/uniruy/appuser/PacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uniruy.appuser;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PacienteRepository extends JpaRepository<Paciente, Long> {
-    List<Paciente> findByMedicoId(Long medicoId);
-    Long countByMedicoId(Long medicoId);
-}
